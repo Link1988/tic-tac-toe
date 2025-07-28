ff96c770925e964f6416d5b9c8131fbc9a6f9ad3
Alejandro Hernandez Ramirez <alez.hdz.88@gmail.com>
2025-07-28 11:58:43 -0600

    add hover

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -10,7 +10,7 @@ export default function Square({ value, onClickPosition }: Props) {
   return (
     <button
       onClick={onClickPosition}
-      className="w-24 h-24 sm:w-28 sm:h-28 flex items-center justify-center border-4 border-white rounded-xl transition-all duration-300 ease-in-out text-white text-5xl sm:text-6xl font-bold hover:scale-105"
+      className="w-24 h-24 sm:w-28 sm:h-28 flex items-center justify-center border-4 border-white rounded-xl transition-all duration-300 ease-in-out text-white text-5xl sm:text-6xl font-bold hover:scale-105 cursor-pointer"
     >
       {value && (
         <span className="animate-scale-in">
